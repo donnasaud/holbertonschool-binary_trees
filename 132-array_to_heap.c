5d043da3ac0bef9977fef9cf2b0385c7b88d5e39
donnasaud <donnaalmadani@gmail.com>
2025-04-12 20:39:59 +0000

    task 39 final fixes

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -5,8 +5,11 @@
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  *
- * Return: Pointer to the root node of the created Binary Heap, or NULL on failure
+ * Return: Pointer to the root node of the created Binary Heap,
+ *         or NULL on failure
  */
+
+
 heap_t *array_to_heap(int *array, size_t size)
 {
 	size_t i;
