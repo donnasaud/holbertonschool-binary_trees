75c8784c6500b541b96f18b5f3eb9c1efe9d8e87
donnasaud <donnaalmadani@gmail.com>
2025-04-12 18:17:50 +0000

    task 31 final fixes 2

--- 120-binary_tree_is_avl.c
+++ 120-binary_tree_is_avl.c
@@ -30,7 +30,7 @@ int is_bst(const binary_tree_t *tree, int min, int max)
 	if (tree->n <= min || tree->n >= max)
 		return (0);
 	return (is_bst(tree->left, min, tree->n) &&
-	        is_bst(tree->right, tree->n, max));
+		is_bst(tree->right, tree->n, max));
 }
 
 /**
