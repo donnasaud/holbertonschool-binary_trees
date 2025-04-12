678e22a2c90949fe98cfc8f05c69756dd16228d7
donnasaud <donnaalmadani@gmail.com>
2025-04-12 19:32:11 +0000

    task 35 in binary trees

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+/**
+ * build_avl - Recursively builds an AVL tree from a sorted array
+ * @array: Pointer to the first element of the array
+ * @start: Starting index of the sub-array
+ * @end: Ending index of the sub-array
+ * @parent: Pointer to the parent node
+ *
+ * Return: Pointer to the root node of the created subtree
+ */
+avl_t *build_avl(int *array, int start, int end, avl_t *parent)
+{
+	int mid;
+	avl_t *root;
+
+	if (start > end)
+		return (NULL);
+
+	mid = (start + end) / 2;
+	root = binary_tree_node(parent, array[mid]);
+	if (!root)
+		return (NULL);
+
+	root->left = build_avl(array, start, mid - 1, root);
+	root->right = build_avl(array, mid + 1, end, root);
+
+	return (root);
+}
+
+/**
+ * sorted_array_to_avl - Builds an AVL tree from a sorted array
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ *
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (!array || size == 0)
+		return (NULL);
+
+	return (build_avl(array, 0, (int)size - 1, NULL));
+}
+
