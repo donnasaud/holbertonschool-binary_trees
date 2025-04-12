0c16485d44fdf043febd828e59f8fb59c142d11c
donnasaud <donnaalmadani@gmail.com>
2025-04-12 20:33:54 +0000

    task 38 final fixes

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -2,93 +2,77 @@
 #include <stdlib.h>
 
 /**
- * height - Measures the height of a binary tree
- * @tree: Pointer to the root node
- * Return: Height or 0 if NULL
- */
-static size_t height(const binary_tree_t *tree)
-{
-	size_t left;
-	size_t right;
-
-	if (!tree)
-		return (0);
-
-	left = height(tree->left);
-	right = height(tree->right);
-	return ((left > right ? left : right) + 1);
-}
-
-/**
- * is_perfect - Checks if a binary tree is perfect
- * @tree: Pointer to the root
- * Return: 1 if perfect, 0 otherwise
- */
-static int is_perfect(const binary_tree_t *tree)
-{
-	int left_h, right_h;
-
-	if (!tree)
-		return (0);
-
-	if (!tree->left && !tree->right)
-		return (1);
-	if (!tree->left || !tree->right)
-		return (0);
-
-	left_h = height(tree->left);
-	right_h = height(tree->right);
-
-	return (left_h == right_h &&
-		is_perfect(tree->left) &&
-		is_perfect(tree->right));
-}
-
-/**
- * insert_node - Recursively inserts a new node in the proper place
- * @root: Pointer to current root
+ * level_order_insert - Inserts a node in level-order fashion
+ * @tree: Pointer to current node
  * @value: Value to insert
- * Return: Pointer to inserted node
+ * Return: Pointer to newly inserted node
  */
-static heap_t *insert_node(heap_t *root, int value)
+static heap_t *level_order_insert(heap_t *tree, int value)
 {
-	heap_t *inserted = NULL;
+	heap_t *new = NULL;
+	heap_t *queue[1024];
+	int front = 0, rear = 0;
+	heap_t *current;
 
-	if (!root->left)
+	if (!tree)
+		return (NULL);
+
+	queue[rear++] = tree;
+	while (front < rear)
 	{
-		root->left = binary_tree_node(root, value);
-		inserted = root->left;
+		current = queue[front++];
+
+		if (!current->left)
+		{
+			current->left = binary_tree_node(current, value);
+			new = current->left;
+			break;
+		}
+		else
+			queue[rear++] = current->left;
+
+		if (!current->right)
+		{
+			current->right = binary_tree_node(current, value);
+			new = current->right;
+			break;
+		}
+		else
+			queue[rear++] = current->right;
 	}
-	else if (!root->right)
+
+	return (new);
+}
+
+/**
+ * heapify_up - Restores the Max Heap order by swapping up
+ * @node: Pointer to the inserted node
+ * Return: Pointer to final position
+ */
+static heap_t *heapify_up(heap_t *node)
+{
+	int temp;
+
+	while (node->parent && node->n > node->parent->n)
 	{
-		root->right = binary_tree_node(root, value);
-		inserted = root->right;
+		temp = node->n;
+		node->n = node->parent->n;
+		node->parent->n = temp;
+		node = node->parent;
 	}
-	else if (!is_perfect(root))
-		inserted = insert_node(root->left, value);
-	else
-		inserted = insert_node(root->right, value);
-
-	while (inserted && inserted->parent && inserted->n > inserted->parent->n)
-	{
-		int tmp = inserted->n;
-
-		inserted->n = inserted->parent->n;
-		inserted->parent->n = tmp;
-		inserted = inserted->parent;
-	}
-
-	return (inserted);
+	return (node);
 }
 
 /**
  * heap_insert - Inserts a value into a Max Binary Heap
- * @root: Double pointer to root
+ * @root: Double pointer to root node
  * @value: Value to insert
  * Return: Pointer to the inserted node
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
+	heap_t *inserted;
+
 	if (!root)
 		return (NULL);
 
@@ -98,6 +82,10 @@ heap_t *heap_insert(heap_t **root, int value)
 		return (*root);
 	}
 
-	return (insert_node(*root, value));
+	inserted = level_order_insert(*root, value);
+	if (!inserted)
+		return (NULL);
+
+	return (heapify_up(inserted));
 }
 
